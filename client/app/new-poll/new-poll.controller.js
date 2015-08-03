f944257d637f07ee067b9e7d80508d7519313e94
BlindPrimate <ecleek@gmail.com>
2015-08-03 16:02:27 -0400

    auto method attempt
    Former-commit-id: 7d4afeba1c10b0eb5215d65ea6bd16bc5a267b82

--- client/app/new-poll/new-poll.controller.js
+++ client/app/new-poll/new-poll.controller.js
@@ -1,39 +1,19 @@
 'use strict';
 
 angular.module('votingAppApp')
-  .controller('NewPollCtrl', function ($http, $scope) {
+  .controller('PollsCtrl', function ($scope) {
     $scope.question = "Who was the best Bond?";
-    $scope.poll = {
-      question: $scope.question,
-      options: [
-        {
-          label: "Sean Connery",
-          votes: 0
-        }, {
-          label: "Daniel Craig",
-          votes: 0
-        }, {
-          label: "Roger Moore",
-          votes: 0
-        }
-      ]
-    };
-
+    $scope.options = [
+      "Sean Connery",
+      "Daniel Craig",
+      "Roger Moore"
+    ];
 
     $scope.removeOption = function(index) {
-      $scope.poll.options.splice(index, 1);
+      $scope.options.splice(index, 1);
     };
 
     $scope.addOption = function () {
-      $scope.poll.options.push({
-        label: "Option",
-        votes: 0
-      });
-    };
-
-    $scope.submit = function() {
-      $http.post('/api/polls', $scope.poll).success(function () {
-        console.log('check');
-      });
+      $scope.options.push("option");
     }
   });
