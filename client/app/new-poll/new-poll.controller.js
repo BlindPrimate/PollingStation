ba9383119cccdbdd1d2343cce40840d8b4dbfbbf
BlindPrimate <ecleek@gmail.com>
2015-08-02 23:55:59 -0400

    poll page foundations
    Former-commit-id: 697da388799b7aa0cbfe766353da0f28ed837f18

--- client/app/new-poll/new-poll.controller.js
+++ client/app/new-poll/new-poll.controller.js
@@ -2,5 +2,18 @@
 
 angular.module('votingAppApp')
   .controller('NewPollCtrl', function ($scope) {
-    $scope.test = "knock knock, bitches";
+    $scope.question = "Who was the best Bond?"
+    $scope.options = [
+      "Sean Connery",
+      "Daniel Craig",
+      "Roger Moore"
+    ]
+
+    $scope.removeOption = function(index) {
+      $scope.options.splice(index, 1);
+    }
+
+    $scope.addOption = function () {
+      $scope.options.push("option");   
+    }
   });
