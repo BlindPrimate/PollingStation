72ac3a0c29ebd70fa30c68b14afaef07fcd725f0
BlindPrimate <ecleek@gmail.com>
2015-08-04 14:48:22 -0400

    database updates
    Former-commit-id: 5506641e8e9a5c8a8dc384b9fcb84f8bfe2ec583

--- client/app/polls/poll/poll.controller.js
+++ client/app/polls/poll/poll.controller.js
@@ -3,7 +3,19 @@
 angular.module('votingAppApp')
   .controller('PollCtrl', function ($stateParams, $http, $scope) {
 
-    $http.get('api/polls/' + $stateParams.id).success(function (data) {
+    var pollApi = 'api/polls/' + $stateParams.id;
+
+    $http.get(pollApi).success(function (data) {
       $scope.poll = data;
     });
+
+    $scope.currChoice = '';
+
+    $scope.submitVote = function (optionIndex) {
+      $scope.poll.options[optionIndex].votes += 1;
+      $http.put(pollApi, $scope.poll)
+    }
+
+
+
   });
